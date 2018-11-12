5d8c16eddca5df5842da5aba03702909f4cec89f
danieleverest <>
2018-11-12 23:51:41 +0100

    add constants to improve readabilty and expansivity

--- server/index.js
+++ server/index.js
@@ -1,25 +1,15 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
+const { database: { USER, PASS, ADDRESS, PORT } } = require('./constants');
+
 mongoose.Promise = global.Promise;
-const USER = process.env.DB_USER;
-const PASS = process.env.DB_PASS;
-const ADDRESS = process.env.DB_ADDRESS;
 
 const DB = `mongodb://${USER}:${PASS}${ADDRESS}`;
 
-mongoose
-  .connect(
-    DB,
-    { useNewUrlParser: true }
-  )
+mongoose.connect(DB, { useNewUrlParser: true })
   .then(() => console.log('Database Connected!'))
-  .catch(() =>
-    console.log('Unable to connect to database. Please check credentials')
-  );
+  .catch(() => console.log('Unable to connect to database. Please check credentials'));
 
 const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
