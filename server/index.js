68d98de4a256db3c05bf6e63e3a9e49adfee8961
danieleverest <>
2018-11-12 20:35:23 +0000

    connects to cloud mongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const http = require('http');
 
 mongoose.Promise = global.Promise;
+const USER = process.env.DB_USER;
+const PASS = process.env.DB_PASS;
+const ADDRESS = process.env.DB_ADDRESS;
 
-mongoose.connect('mongodb://localhost:27017/coding_resources', { useNewUrlParser: true }).then(() => console.log('Database Connected!'));
+const DB = `mongodb://${USER}:${PASS}${ADDRESS}`;
+
+mongoose
+  .connect(
+    DB,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Database Connected!'))
+  .catch(() =>
+    console.log('Unable to connect to database. Please check credentials')
+  );
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer(app);
-
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
