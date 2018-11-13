a9e5f2d0d5b07b916d611007fcb7f7b08c3d9cf8
danieleverest <>
2018-11-13 17:30:54 +0000

    encrypting passwords with argon2

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Users = new Schema({
-  username: { type: String, required: true },
-  user_id: { type: String, required: true },
+  username: { type: String, required: true, unique: true, minlength: 5 },
+  password: { type: String, required: true, minlength: 6 },
   links: { type: Array },
   saved: { type: Array },
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,21 +8,14 @@ router.post('/signup', async (req, res) => {
     const { username, password } = req.body;
     if (password.length < 6) res.json({ password: 'Password must be 6 or more characters' });
 
-    // const user = argon2.hash(password)
-    //   .then((hash) => {
-    //     return new User({
-    //       username,
-    //       password: hash,
-    //     });
-    //   });
+    const hash = await argon2.hash(password)
     const user = new Users({
       username,
-      password,
+      password: hash,
     });
     const newUser = await user.save();
-    res.json(newUser);
-  }
-  catch (error) {
+    res.status(200).json(newUser);
+  } catch (error) {
     res.status(400).json(error);
   }
 });
