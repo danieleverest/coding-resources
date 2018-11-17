29efa3300fffa136f9a1684cc735e4a2c0c8a623
danieleverest <>
2018-11-16 21:20:10 -0600

    update

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Menu } from './components';
 import './App.scss';
+import LinkListPage from './components/LinkListPage/LinkListPage';
+
 
 const app = () => (
   <div className="App">
     <Menu />
+    <LinkListPage />
   </div>
 );
 
